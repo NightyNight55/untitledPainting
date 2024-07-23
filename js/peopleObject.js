d13552c3e842cd771fe6df1bf664115d6bd94c52
恩 <151903515+NightyNight55@users.noreply.github.com>
2024-07-23 21:42:31 +0800

    update 20240723

--- js/peopleObject.js
+++ js/peopleObject.js
@@ -1,147 +1,147 @@
 const peopleObject = [
     {
-        frontImageSrc: '0-1正面.png',
-        backImageSrc: '0-1背面.png'
+        frontImageSrc: '0-1f.png',
+        backImageSrc: '0-1b.png'
     },
     {
-        frontImageSrc: '0-2正面.png',
-        backImageSrc: '0-2背面.png'
+        frontImageSrc: '0-2f.png',
+        backImageSrc: '0-2b.png'
     },
     {
-        frontImageSrc: '1-1正面.png',
-        backImageSrc: '1-1背面.png'
+        frontImageSrc: '1-1f.png',
+        backImageSrc: '1-1b.png'
     },
     {
-        frontImageSrc: '1-2正面.png',
-        backImageSrc: '1-2背面.png'
+        frontImageSrc: '1-2f.png',
+        backImageSrc: '1-2b.png'
     },
     {
-        frontImageSrc: '1-3正面.png',
-        backImageSrc: '1-3背面.png'
+        frontImageSrc: '1-3f.png',
+        backImageSrc: '1-3b.png'
     },
     {
-        frontImageSrc: '1-4正面.png',
-        backImageSrc: '1-4背面.png'
+        frontImageSrc: '1-4f.png',
+        backImageSrc: '1-4b.png'
     },
     {
-        frontImageSrc: '1-5正面.png',
-        backImageSrc: '1-5背面.png'
+        frontImageSrc: '1-5f.png',
+        backImageSrc: '1-5b.png'
     },
     {
-        frontImageSrc: '1-6正面.png',
-        backImageSrc: '1-6背面.png'
+        frontImageSrc: '1-6f.png',
+        backImageSrc: '1-6b.png'
     },
     {
-        frontImageSrc: '1-7正面.png',
-        backImageSrc: '1-7背面.png'
+        frontImageSrc: '1-7f.png',
+        backImageSrc: '1-7b.png'
     },
     {
-        frontImageSrc: '2-1正面.png',
-        backImageSrc: '2-1背面.png'
+        frontImageSrc: '2-1f.png',
+        backImageSrc: '2-1b.png'
     },
     {
-        frontImageSrc: '2-2正面.png',
-        backImageSrc: '2-2背面.png'
+        frontImageSrc: '2-2f.png',
+        backImageSrc: '2-2b.png'
     },
     {
-        frontImageSrc: '2-3正面.png',
-        backImageSrc: '2-3背面.png'
+        frontImageSrc: '2-3f.png',
+        backImageSrc: '2-3b.png'
     },
     {
-        frontImageSrc: '2-4正面.png',
-        backImageSrc: '2-4背面.png'
+        frontImageSrc: '2-4f.png',
+        backImageSrc: '2-4b.png'
     },
     {
-        frontImageSrc: '2-5正面.png',
-        backImageSrc: '2-5背面.png'
+        frontImageSrc: '2-5f.png',
+        backImageSrc: '2-5b.png'
     },
     {
-        frontImageSrc: '2-6正面.png',
-        backImageSrc: '2-6背面.png'
+        frontImageSrc: '2-6f.png',
+        backImageSrc: '2-6b.png'
     },
     {
-        frontImageSrc: '2-7正面.png',
-        backImageSrc: '2-7背面.png'
+        frontImageSrc: '2-7f.png',
+        backImageSrc: '2-7b.png'
     },
     {
-        frontImageSrc: '2-8正面.png',
-        backImageSrc: '2-8背面.png'
+        frontImageSrc: '2-8f.png',
+        backImageSrc: '2-8b.png'
     },
     {
-        frontImageSrc: '2-9正面.png',
-        backImageSrc: '2-9背面.png'
+        frontImageSrc: '2-9f.png',
+        backImageSrc: '2-9b.png'
     },
     {
-        frontImageSrc: '2-10正面.png',
-        backImageSrc: '2-10背面.png'
+        frontImageSrc: '2-10f.png',
+        backImageSrc: '2-10b.png'
     },
     {
-        frontImageSrc: '2-11正面.png',
-        backImageSrc: '2-11背面.png'
+        frontImageSrc: '2-11f.png',
+        backImageSrc: '2-11b.png'
     },
     {
-        frontImageSrc: '2-12正面.png',
-        backImageSrc: '2-12背面.png'
+        frontImageSrc: '2-12f.png',
+        backImageSrc: '2-12b.png'
     },
     {
-        frontImageSrc: '3-1正面.png',
-        backImageSrc: '3-1背面.png'
+        frontImageSrc: '3-1f.png',
+        backImageSrc: '3-1b.png'
     },
     {
-        frontImageSrc: '3-2正面.png',
-        backImageSrc: '3-2背面.png'
+        frontImageSrc: '3-2f.png',
+        backImageSrc: '3-2b.png'
     },
     {
-        frontImageSrc: '3-3正面.png',
-        backImageSrc: '3-3背面.png'
+        frontImageSrc: '3-3f.png',
+        backImageSrc: '3-3b.png'
     },
     {
-        frontImageSrc: '3-4正面.png',
-        backImageSrc: '3-4背面.png'
+        frontImageSrc: '3-4f.png',
+        backImageSrc: '3-4b.png'
     },
     {
-        frontImageSrc: '3-5正面.png',
-        backImageSrc: '3-5背面.png'
+        frontImageSrc: '3-5f.png',
+        backImageSrc: '3-5b.png'
     },
     {
-        frontImageSrc: '3-6正面.png',
-        backImageSrc: '3-6背面.png'
+        frontImageSrc: '3-6f.png',
+        backImageSrc: '3-6b.png'
     },
     {
-        frontImageSrc: '4-1正面.png',
-        backImageSrc: '4-1背面.png'
+        frontImageSrc: '4-1f.png',
+        backImageSrc: '4-1b.png'
     },
     {
-        frontImageSrc: '4-2正面.png',
-        backImageSrc: '4-2背面.png'
+        frontImageSrc: '4-2f.png',
+        backImageSrc: '4-2b.png'
     },
     {
-        frontImageSrc: '4-3正面.png',
-        backImageSrc: '4-3背面.png'
+        frontImageSrc: '4-3f.png',
+        backImageSrc: '4-3b.png'
     },
     {
-        frontImageSrc: '4-4正面.png',
-        backImageSrc: '4-4背面.png'
+        frontImageSrc: '4-4f.png',
+        backImageSrc: '4-4b.png'
     },
     {
-        frontImageSrc: '4-5正面.png',
-        backImageSrc: '4-5背面.png'
+        frontImageSrc: '4-5f.png',
+        backImageSrc: '4-5b.png'
     },
     {
-        frontImageSrc: '4-6正面.png',
-        backImageSrc: '4-6背面.png'
+        frontImageSrc: '4-6f.png',
+        backImageSrc: '4-6b.png'
     },
     {
-        frontImageSrc: '5-1正面.png',
-        backImageSrc: '5-1背面.png'
+        frontImageSrc: '5-1f.png',
+        backImageSrc: '5-1b.png'
     },
     {
-        frontImageSrc: '5-2正面.png',
-        backImageSrc: '5-2背面.png'
+        frontImageSrc: '5-2f.png',
+        backImageSrc: '5-2b.png'
     },
     {
-        frontImageSrc: '5-3正面.png',
-        backImageSrc: '5-3背面.png'
+        frontImageSrc: '5-3f.png',
+        backImageSrc: '5-3b.png'
     },
     {
         frontImageSrc: 'S.png',
